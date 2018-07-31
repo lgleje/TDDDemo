3efbbc55ee44f310b67c1050c777e8d135bbed1d
zhanglejie <zhanglejie-os@360os.com>
2018-07-31 11:40:56 +0800

    Remove reductant public modifier

--- app/src/main/java/com/example/zhanglejie_os/tdddemo/MvpContract.java
+++ app/src/main/java/com/example/zhanglejie_os/tdddemo/MvpContract.java
@@ -1,12 +1,12 @@
 package com.example.zhanglejie_os.tdddemo;
 
 public interface MvpContract {
-    public interface Presenter {
-        public void handleClickSignin();
-        public void handleClickSignup();
+    interface Presenter {
+        void handleClickSignin();
+        void handleClickSignup();
     }
-    public interface MvpView{
-        public void onClickSignin();
-        public void onClickSighup();
+    interface MvpView{
+        void onClickSignin();
+        void onClickSighup();
     }
 }
